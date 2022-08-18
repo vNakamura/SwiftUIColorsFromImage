98ca98bb6ebac814eee257895768bed6a0c36905
Vinicius Nakamura <hello@vnakamura.com>
2022-08-18 17:51:45 -0300

    Initial Commit

---
+++ SwiftUIColorsFromImage/SwiftUIColorsFromImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorsFromImageApp.swift
+//  SwiftUIColorsFromImage
+//
+//  Created by Vinicius Nakamura on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorsFromImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
