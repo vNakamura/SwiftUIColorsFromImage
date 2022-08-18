b07503c261bb27cafbc8b86b15c3237c81636b0e
Vinicius Nakamura <hello@vnakamura.com>
2022-08-18 19:14:11 -0300

    Show grid of images loaded from Lorem Picsum

--- SwiftUIColorsFromImage/ContentView.swift
+++ SwiftUIColorsFromImage/ContentView.swift
@@ -8,14 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var thumbnails = PicsumList()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            switch thumbnails.status {
+            case .idle, .loading:
+                ProgressView()
+                    .controlSize(.large)
+                    .padding(.top, 100)
+            case .error(let message):
+                VStack(alignment: .center) {
+                    Image(systemName: "x.square.fill")
+                        .font(.title)
+                        .imageScale(.large)
+                        .padding(.top, 100)
+                    Text(message)
+                }
+                .foregroundColor(.secondary)
+            case .complete:
+                grid
+            }
         }
-        .padding()
+        .safeAreaInset(edge: .top, spacing: 0) {
+            Text("My Gallery")
+                .padding(.vertical, 20)
+                .frame(maxWidth: .infinity)
+                .background(
+                    .regularMaterial,
+                    ignoresSafeAreaEdges: [.top]
+                )
+                .foregroundColor(.secondary)
+                .font(.fancy(size: 36).bold())
+        }
+        .task(thumbnails.loadPage)
+        .refreshable(action: thumbnails.loadPage)
+    }
+
+    var grid: some View {
+        LazyVGrid(
+            columns: [GridItem(.adaptive(minimum: 140), spacing: 0)],
+            spacing: 0
+        ) {
+            ForEach(thumbnails.photos) { photo in
+                AsyncImage(url: URLs.squareImageByID(photo.id, size: 500)) { image in
+                    VStack {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.horizontal, 20)
+                            .padding(.top, 20)
+                        Text(photo.author)
+                            .font(.fancy(size: 18))
+                            .lineLimit(1)
+                            .padding(.horizontal, 10)
+                            .padding(.bottom, 10)
+                    }
+                } placeholder: {
+                    VStack {
+                        ProgressView()
+                            .frame(maxHeight: .infinity)
+                            .aspectRatio(1, contentMode: .fit)
+                            .padding(.horizontal, 20)
+                            .padding(.top, 20)
+                        Text(photo.author)
+                            .font(.fancy(size: 18))
+                            .padding(.horizontal, 10)
+                            .padding(.bottom, 10)
+                            .hidden()
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension Font {
+    static func fancy(size: CGFloat) -> Font {
+        Font.custom("Bodoni 72", size: size)
     }
 }
 
---
+++ SwiftUIColorsFromImage/Picsum.swift
@@ -0,0 +1,90 @@
+//
+//  Picsum.swift
+//  SwiftUIColorsFromImage
+//
+//  Created by Vinicius Nakamura on 18/08/22.
+//
+
+import Foundation
+import SwiftUI
+
+enum URLs {
+    private static let base = URLComponents(string: "https://picsum.photos")
+    
+    static func list(page: Int) -> URL? {
+        guard var components = base else { return nil }
+        components.path = "/v2/list"
+        components.queryItems = [URLQueryItem(name: "page", value: String(page))]
+        return components.url
+    }
+    
+    static func squareImageByID(_ id: String, size: Int) -> URL? {
+        guard var components = base else { return nil }
+        components.path = "/id/\(id)/\(size)"
+        return components.url
+    }
+}
+
+struct PictureInfo: Identifiable, Decodable {
+    let id: String
+    let author: String
+    let width: Int
+    let height: Int
+    let url: String
+}
+
+enum Status {
+    case idle, loading, complete
+    case error(message: String)
+}
+
+enum PicsumError: Error {
+    case urlError, networkError, decodeError
+}
+
+extension PicsumError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .urlError:
+            return NSLocalizedString("Invalid URL", comment: "PicsumError")
+        case .networkError:
+            return NSLocalizedString("Could not reach the server", comment: "PicsumError")
+        case .decodeError:
+            return NSLocalizedString("Invalid response", comment: "PicsumError")
+        }
+    }
+}
+
+enum PicsumService {
+    static func fetchList(_ page: Int = 1) async throws -> [PictureInfo] {
+        guard let url = URLs.list(page: page)
+        else { throw PicsumError.urlError }
+        
+        guard let (data, _) = try? await URLSession.shared.data(from: url)
+        else { throw PicsumError.networkError }
+        
+        guard let decoded = try? JSONDecoder().decode([PictureInfo].self, from: data)
+        else { throw PicsumError.decodeError }
+        
+        return decoded
+    }
+}
+
+@MainActor
+class PicsumList: ObservableObject {
+    @Published var photos: [PictureInfo] = []
+    @Published var status: Status = .idle
+    
+    @Sendable func loadPage() async {
+        let page = Int.random(in: 1 ... 33)
+        
+        photos = []
+        status = .loading
+        do {
+            photos = try await PicsumService.fetchList(page)
+            status = .complete
+        } catch {
+            status = .error(message: error.localizedDescription)
+        }
+    }
+}
